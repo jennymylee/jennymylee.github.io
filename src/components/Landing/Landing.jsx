436c6bc4372f0f333dae638b176f7fc99c202b27
Aligary <aligaryjr@gmail.com>
2022-07-19 15:28:47 -0700

    skeleton for Landing page

---
+++ src/components/Landing/Landing.jsx
@@ -0,0 +1,20 @@
+import * as React from "react"
+import "./Landing.css"
+
+export default function Landing() {
+
+  return (
+    <div className="landing-page">
+        <div className="Hero">
+            <div className="Header">
+                <h1>LOREMIPS</h1>
+                <p>Lorem ipsum dolor adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim ve_niam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </div>
+            <div className="Hero-pic">
+                <img src="img.png" alt="Hero Pic"/>
+            </div>
+            
+        </div>
+    </div>
+  )
+}
