8e539b7a70b8f0de735a78f161ac067a73cc61e8
jennymylee <jennylee038@gmail.com>
2022-07-20 10:01:42 -0700

    styled profile page

--- src/components/ProfilePage/ProfilePage.jsx
+++ src/components/ProfilePage/ProfilePage.jsx
@@ -1,11 +1,18 @@
 import * as React from "react";
 import "./ProfilePage.css";
 import ProfileSideBar from "../ProfileSideBar/ProfileSideBar";
+import ProfileBanner from "../ProfileBanner/ProfileBanner";
+import Wishlist from "../Wishlist/Wishlist";
 
 export default function ProfilePage() {
   return (
     <div className="profile-page">
       <ProfileSideBar />
+      <div className="profile">
+        <h1 className="profile-title">My Profile</h1>
+        <ProfileBanner />
+        <Wishlist />
+      </div>
     </div>
   );
 }
--- src/components/Wishlist/Wishlist.jsx
+++ src/components/Wishlist/Wishlist.jsx
@@ -2,5 +2,12 @@ import * as React from "react";
 import "./Wishlist.css";
 
 export default function Wishlist() {
-  return <div></div>;
+  return (
+    <div className="wishlist">
+      <div className="wishlist-tabs">
+        <button className="wishlist-btn">Wishlist</button>
+      </div>
+      <div className="wishlist-items"></div>
+    </div>
+  );
 }
