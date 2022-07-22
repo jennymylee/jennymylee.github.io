680ba489377ff534d2102b83c148c7ee26fb50dc
jennymylee <jennylee038@gmail.com>
2022-07-22 10:00:43 -0700

    added apiclient

--- src/services/apiClient.js
+++ src/services/apiClient.js
@@ -1,40 +1,41 @@
-import axios from "axios"
+import axios from "axios";
 
-class ApiClient{
-    constructor(remoteHostUrl){
-        this.remoteHostUrl = remoteHostUrl
-        this.token = null
-        this.tokenName = "sneaker_store_token"
+class ApiClient {
+  constructor(remoteHostUrl) {
+    this.remoteHostUrl = remoteHostUrl;
+    this.token = null;
+    this.tokenName = "sneaker_store_token";
+  }
+
+  async setToken(token) {
+    this.token = token;
+    localStorage.setItem(this.tokenName, token);
+  }
+
+  async request({ endpoint, method = `GET`, data = {} }) {
+    const url = `${this.remoteHostUrl}/${endpoint}`;
+
+    const headers = {
+      "Content-Type": "application/json",
+      user_id: data,
+    };
+
+    if (this.token) {
+      headers["Authorization"] = `Bearer ${this.token}`;
     }
 
-    async setToken(token){
-        this.token = token
-        localStorage.setItem(this.tokenName, token)
+    try {
+      const res = await axios({ url, method, data, headers });
+      return { data: res.data, error: null };
+    } catch (error) {
+      console.error({ errorResponse: error.response });
+      // const message = error.response.data.error.message
+      const message = "error found";
+      return { data: null, error: message || String(error) };
     }
+  }
 
-    async request({ endpoint, method = `GET`, data = {}}){
-        const url = `${this.remoteHostUrl}/${endpoint}`
-
-        const headers = {
-            "Content-Type": "application/json",
-            "user_id": data
-        }
-
-        if(this.token){
-            headers["Authorization"] = `Bearer ${this.token}`
-        }
-
-        try{
-            const res = await axios({url, method, data, headers})
-            return {data: res.data, error: null}
-        }catch(error){
-            console.error({errorResponse: error.response})
-            const message = error.response.data.error.message
-            return { data: null, error: message || String(error)}
-        }
-    }
-
-    /*
+  /*
     Still needs testing
     
 
@@ -56,13 +57,21 @@ class ApiClient{
 
     */
 
-    async loginUser(credentials){
-        return await this.request({endpoint: `auth/login`, method: `POST`, data: credentials})
-    }
+  async loginUser(credentials) {
+    return await this.request({
+      endpoint: `auth/login`,
+      method: `POST`,
+      data: credentials,
+    });
+  }
 
-    async signUpUser(credentials){
-        return await this.request({endpoint: `auth/register`, method: `POST`, data: credentials})
-    }
+  async signUpUser(credentials) {
+    return await this.request({
+      endpoint: `auth/register`,
+      method: `POST`,
+      data: credentials,
+    });
+  }
 }
 
-export default new ApiClient("http://localhost:3001")
+export default new ApiClient("http://localhost:3001");
