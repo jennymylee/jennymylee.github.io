71edadb2ba733fcd73790b8f68390be34cec1e83
jennymylee <jennylee038@gmail.com>
2022-07-25 09:57:50 -0700

    started coding trending

--- src/components/TrendingPage/TrendingPage.jsx
+++ src/components/TrendingPage/TrendingPage.jsx
@@ -5,14 +5,36 @@ import TrendingProducts from "../TrendingProducts/TrendingProducts";
 import { useAuthContext } from "../../contexts/auth";
 
 export default function TrendingPage() {
-  const { user } = useAuthContext();
+  const [trendingProducts, setTrendingProducts] = React.useState([]);
+  const { user, apiClient } = useAuthContext();
+  const [error, setError] = React.useState({});
   console.log("user in trending page", user);
+
+  React.useEffect(() => {
+    const getTrending = async () => {
+      let trendingArray = [];
+      try {
+        if (user) {
+          trendingArray = await apiClient.getTrendingProducts();
+          setTrendingProducts(trendingArray.products);
+        }
+      } catch (err) {
+        console.log("error in TrendingProducts:", err);
+        setError(err.message);
+      }
+    };
+    getTrending();
+  }, []);
+
   return (
     <div className="trending-page">
-      <TrendingSideBar />
+      <TrendingSideBar
+        trendingProducts={trendingProducts}
+        setTrendingProducts={setTrendingProducts}
+      />
       <div className="trending">
         <h1 className="trending-title">What's Trending</h1>
-        <TrendingProducts />
+        <TrendingProducts trendingProducts={{ trendingProducts }} />
       </div>
     </div>
   );
--- src/components/TrendingProducts/TrendingProducts.jsx
+++ src/components/TrendingProducts/TrendingProducts.jsx
@@ -1,10 +1,40 @@
 import * as React from "react";
 import "./TrendingProducts.css";
+import { useAuthContext } from "../../contexts/auth";
+import ApiClient from "../../services/apiClient";
 
-export default function TrendingProducts() {
+export default function TrendingProducts({ trendingProducts }) {
+  // const [trendingProducts, setTrendingProducts] = React.useState([]);
+  // const [error, setError] = React.useState({});
+  // const { user, apiClient } = useAuthContext();
+  // React.useEffect(() => {
+  //   const getTrending = async () => {
+  //     let trendingArray = [];
+  //     try {
+  //       if (user) {
+  //         trendingArray = await apiClient.getTrendingProducts();
+  //         setTrendingProducts(trendingArray.products);
+  //       }
+  //     } catch (err) {
+  //       console.log("error in TrendingProducts:", err);
+  //       setError(err.message);
+  //     }
+  //   };
+  //   getTrending();
+  // }, []);
+
+  const tp = () => {
+    if (trendingProducts.trendingProducts) {
+      console.log("trendingProducts", trendingProducts.trendingProducts);
+      return trendingProducts.trendingProducts[0].name;
+    } else {
+      return;
+    }
+  };
+
   return (
     <div className="trending-products">
-      <div className="tp-items"></div>
+      <div className="tp-items">{() => tp()}</div>
     </div>
   );
 }
