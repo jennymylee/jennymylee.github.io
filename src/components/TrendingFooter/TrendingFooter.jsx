46192c9bfc1b2e034dd4c39af92e67c91f8ffd01
jennymylee <jennylee038@gmail.com>
2022-07-27 14:56:24 -0700

    working on footer

---
+++ src/components/TrendingFooter/TrendingFooter.jsx
@@ -0,0 +1,33 @@
+import * as React from "react";
+import "./TrendingFooter.css";
+
+export default function TrendingFooter() {
+  return (
+    <div className="trending-footer">
+      {/* <div className="footer-head">
+        <div className="footer-title">Contact</div>
+        <div className="footer-logo">
+          <i className="fa-brands fa-connectdevelop"></i>
+        </div>
+      </div>
+      <div className="footer-description">
+        <p>
+          Lorem ipsum dolor adipiscing elit, sed do eiusmod tempor incididunt ut
+          labore et dolore magna aliqua.
+        </p>
+      </div>
+      <div className="footer-end">
+        <div className="footer-social">
+          <i className="fa-brands fa-facebook fa-xl contact"></i>
+          <i className="fa-brands fa-instagram fa-xl contact"></i>
+          <i className="fa-brands fa-twitter fa-xl contact"></i>
+          <i className="fa-brands fa-linkedin fa-xl contact"></i>
+          <i className="fa-regular fa-envelope fa-xl contact"></i>
+        </div>
+        <div className="footer-copy">
+          <p>Copyright © 2022 Lorem Ipsum all rights reserved</p>
+        </div>
+      </div> */}
+    </div>
+  );
+}
