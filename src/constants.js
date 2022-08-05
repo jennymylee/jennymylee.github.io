ec73a65f2d6a63de0ae1c03762c4293150794e22
Aligary <aligaryjr@gmail.com>
2022-08-05 08:34:56 -0700

    fixed base URL again

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,6 @@
 
 //const API_BASE_URL = "http://localhost:3001";
- const API_BASE_URL = "http://elevate-sneakers.herokuapp.com";
+ const API_BASE_URL = "//elevate-sneakers.herokuapp.com";
 module.exports = {
     API_BASE_URL
 }
